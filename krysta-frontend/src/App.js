99a94e8e3704ab08446ed6eb91113c1f6b310645
DT7Solutions <developer@dt7solutions.com>
2023-03-29 13:31:54 +0530

    fixed data

--- krysta-frontend/src/App.js
+++ krysta-frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route path="login"element={<Login />} />
+            <Route index element={<Login />} />
             <Route path="home" element={<Home />} />
             <Route path="users">
               <Route index element={<List />} />
